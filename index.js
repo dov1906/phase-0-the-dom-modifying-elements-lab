b28ef09c1aa9e5de1a26c9c4db3027b82ad9646d
Dov Azogui <dov.azogui@gmail.com>
2024-08-15 00:00:55 +0200

    changed the index.js content

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+const child = document.querySelector("main")
+child.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent= "Dov is the champion"
